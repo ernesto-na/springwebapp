d7a55cc0066e319224feab84ebf56aa8cae4fdb2
Ernesto <ernesto@MacBook-Air-de-Ernesto.local>
2019-06-16 12:56:26 -0500

    actual

--- src/com/novellius/controller/AdminController.java
+++ src/com/novellius/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.novellius.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,10 +11,15 @@ import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.novellius.pojo.Admin;
+import com.novellius.service.AdminService;
 
 @Controller
 public class AdminController {
 
+	@Autowired
+	private AdminService adminService;
+	
+	
 	@RequestMapping("/admin")
 	public String showAdmin(Model model,
 			@ModelAttribute("resultado")String resultado) {
@@ -23,18 +29,16 @@ public class AdminController {
 		model.addAttribute("resultado",resultado);
 		return "admin";
 	}
-	@RequestMapping(value="/admin/save",method=RequestMethod.POST)
-	public String handleAdmin(@ModelAttribute("admin") Admin adminForm,Model model,
-			RedirectAttributes ra,
-			@RequestParam("estado") String estado) {
-		
-		//model.addAttribute("adminForm", adminForm);
-		System.out.println(adminForm);
-		System.out.println("requestParam: "+estado);
-		
-		ra.addFlashAttribute("resultado","redireccion con exito");
-		
+
+	@RequestMapping(value = "/admin/save", method = RequestMethod.POST)
+	public String handleAdmin(@ModelAttribute("admin") Admin adminForm, Model model, RedirectAttributes ra) {
+
+		if (adminService.save(adminForm)) {
+			ra.addFlashAttribute("resultado", "se guarda correctamente");
+		}else {
+			ra.addFlashAttribute("resultado", "error al guardar");
+		}
 		return "redirect:/admin";
 	}
-	
+
 }
--- src/com/novellius/service/AdminService.java
+++ src/com/novellius/service/AdminService.java
@@ -1,14 +1,28 @@
 package com.novellius.service;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.novellius.dao.AdminDao;
+import com.novellius.pojo.Admin;
+import java.sql.Timestamp;
 
 @Service
 public class AdminService {
 
 	@Autowired
-	private AdminDao adminDao;
+	public AdminDao adminDao;
+	
+	public Boolean save(Admin admin) {
+		
+		admin.setFechaCreacion(new Timestamp(new Date().getTime()));
+		
+		System.out.println(admin.getFechaCreacion());
+		
+		return adminDao.save(admin);
+	}
 	
 }
